52b8dcaba5b8650ec0aa52384cb3c6d823010520
The-Olive <lpierrelouis17@mail.valenciacollege.edu>
2024-09-16 19:47:15 -0400

    Second Commit

--- function.py
+++ function.py
@@ -5,28 +5,29 @@
 
 
 ## Area of the circle
-#radiusInput = int(input("What is the radius of the circle? \n"))
-#areaOfCircle = round(3.141592 * radiusInput ** 2, 2)
+radiusInput = int(input("What is the radius of the circle? \n"))
+areaOfCircle = round(3.141592 * radiusInput ** 2, 2)
 
-#def circleArea(Pi, radiusInput):
-#    print("The area of the circle is {} cm2.".format(areaOfCircle))
+def circleArea(Pi, radiusInput):
+    print("The area of the circle is {} cm2.".format(areaOfCircle))
 
-#Pi = 3.14
+Pi = 3.14
 
-#circleArea(Pi, radiusInput)
-#print("")
+circleArea(Pi, radiusInput)
+print("")
 
 
 ## Amount of taxes owed
-#moneyInput = int(input("What is your amount of money? \n"))
-#taxRate = float(input("What is the tax rate in decimal? Ex: 0.08 \n"))
-#taxOwed = moneyInput + (moneyInput * taxRate)
+moneyInput = int(input("What is your amount of money? \n"))
+taxRate = float(input("What is the tax rate in decimal? Ex: 0.08 \n"))
+taxOwed = moneyInput + (moneyInput * taxRate)
 
-#def taxAmount(moneyInput, taxRate):
-#    print("Your tax amount is ${}.".format(taxOwed))
+def taxAmount(moneyInput, taxRate):
+    print("Your tax amount is ${}.".format(taxOwed))
 
-#taxAmount(taxOwed, taxRate)
+taxAmount(taxOwed, taxRate)
 
+print("")
 
 ## Converting Fahrenheit to Celsius temperature
 fahrenheitInput = int(input("What is the temperature in Fahrenheit? \n"))
